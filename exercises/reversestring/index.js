155bb4e128c714523404e5f96371f040e9c4cff3
evan bechtol <evanbechtol@gmail.com>
2020-06-09 13:53:02 -0500

    Reverse String Exercise
    - Add `reverse()`
    - Add `reverse2()`
    - Add `reverse3()`

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -2,10 +2,29 @@
 // Given a string, return a new string with the reversed
 // order of characters
 // --- Examples
-//   reverse('apple') === 'leppa'
+//   reverse('apple') === 'eLppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse ( str ) {
+  return str ? str.split( "" ).reverse().join( "" ) : "";
+}
+
+function reverse2 ( str ) {
+  const splitStr = str.split( "" );
+  let reverseStr = "";
+
+  for ( let i = splitStr.length - 1; i >= 0; i-- ) {
+    reverseStr += splitStr[ i ];
+  }
+
+  return reverseStr;
+}
+
+function reverse3 ( str ) {
+  return str
+    .split( "" )
+    .reduce( ( rev, char ) => char + rev, "" );
+}
 
 module.exports = reverse;
