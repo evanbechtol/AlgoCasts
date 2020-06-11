cca9c16f181838bb42d6ff09636c20dab7df7861
evan bechtol <evanbechtol@gmail.com>
2020-06-11 13:51:13 -0500

    Array chunking Exercise
    - Add solution

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,32 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk ( array, size ) {
+  const chunkedArr = [];
+
+  array.forEach( elem => {
+    const last = chunkedArr[ chunkedArr.length - 1 ];
+
+    if ( !last || last.length === size ) {
+      chunkedArr.push( [ elem ] );
+    } else {
+      last.push( elem );
+    }
+  } );
+
+  return chunkedArr;
+}
+
+function chunk2 ( array, size ) {
+  const chunkedArr = [];
+  let index = 0;
+
+  while (index < array.length) {
+    chunkedArr.push( array.slice( index, index + size ) );
+    index += size;
+  }
+
+  return chunkedArr;
+}
 
 module.exports = chunk;
