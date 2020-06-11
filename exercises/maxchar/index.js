8245d84110b682c3fca97a77a6831abde5480899
evan bechtol <evanbechtol@gmail.com>
2020-06-11 10:00:50 -0500

    Maxchar Exercise
    - Add solution

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,24 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar ( str ) {
+  const charMap = new Map();
+  let max = 0;
+  let maxChar = "";
+
+  for ( const char of str ) {
+    const value = charMap.get( char ) + 1 || 1;
+    charMap.set( char, value );
+  }
+
+  charMap.forEach( ( value, key ) => {
+    if ( value > max ) {
+      max = value;
+      maxChar = key;
+    }
+  } );
+
+  return maxChar;
+}
 
 module.exports = maxChar;
