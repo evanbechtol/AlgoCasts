c7661d714f7959e0f4460f436798bd4551305562
evan bechtol <evanbechtol@gmail.com>
2020-06-15 16:53:07 -0500

    Anagrams Exercise
    - Add solution

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,51 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams ( stringA, stringB ) {
+  return cleanString( stringA ) === cleanString( stringB );
+}
+
+function cleanString ( str ) {
+  return str
+    .replace( /[^\w]/g, "" )
+    .toLowerCase()
+    .split( "" )
+    .sort()
+    .join( "" );
+}
+
+
+function anagrams1 ( stringA, stringB ) {
+  // replace all non-alphanumeric characters with empty string
+  const alphaNumericRegex = new RegExp( /[^\w]/g );
+  const cleanStringA = stringA.replace( alphaNumericRegex, "" ).toLowerCase();
+  const cleanStringB = stringB.replace( alphaNumericRegex, "" ).toLowerCase();
+  const charMapA = createCharacterMap( cleanStringA );
+  const charMapB = createCharacterMap( cleanStringB );
+  let isAnagram;
+
+  // Verify that both maps have the same number of keys
+  if ( Object.keys( charMapA ).length !== Object.keys( charMapB ).length ) {
+    isAnagram = false;
+  } else {
+    const keysA = Object.keys( charMapA );
+
+    // Using find, see if there are any keys with unequal values.
+    // If find returns "undefined", both maps are equal
+    isAnagram = !keysA.find( key => charMapA[ key ] !== charMapB[ key ] );
+  }
+
+  return isAnagram;
+}
+
+function createCharacterMap ( str ) {
+  const charMap = {};
+
+  for ( const char of str ) {
+    charMap[ char ] = charMap[ char ] + 1 || 1;
+  }
+
+  return charMap;
+}
 
 module.exports = anagrams;
