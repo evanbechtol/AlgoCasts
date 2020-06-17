beb2bb6315daa37ba99d0b5a59e4ccdf7338651c
evan bechtol <evanbechtol@gmail.com>
2020-06-17 13:18:56 -0500

    Capitalize Exercise
    - Add solution

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,26 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize ( str ) {
+  return str
+    .split( " " )
+    .map( word => word = word[ 0 ].toUpperCase() + word.slice( 1 ) )
+    .join( " " );
+}
+
+function capitalize2 ( str ) {
+  let previousChar;
+  let word = "";
+
+  for ( let char of str ) {
+    if ( previousChar === " " || !previousChar ) {
+      char = char.toUpperCase();
+    }
+    word += char;
+    previousChar = char;
+  }
+
+  return word;
+}
 
 module.exports = capitalize;
