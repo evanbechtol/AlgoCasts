ae89997655f802cd196b59d0ef8dc5566845f2c6
evan bechtol <evanbechtol@gmail.com>
2020-06-29 16:45:13 -0500

    Pyramid Exercise Pt. 1
    - Add solutions

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -7,13 +7,28 @@
 //   pyramid(1)
 //       '#'
 //   pyramid(2)
-//       ' # '
+//       ' # ' 1
 //       '###'
 //   pyramid(3)
 //       '  #  '
 //       ' ### '
 //       '#####'
+//   pyramid(4)
+//       r0 '   #   ' i=0 #=i*2+1
+//       r1 '  ###  ' i=1 #=2+1
+//       r2 ' ##### ' i=2 #=4+1
+//       r3 '#######' i=3 #=6+1
 
-function pyramid(n) {}
+function pyramid ( n ) {
+  //number of spaces on either side = n - (r# + 1) > 0 ? n - (r# + 1) : 0
+  for ( let i = 0; i < n; i++ ) {
+    let str = "";
+    const numSpaces = n - ( i + 1 ) > 0 ? n - ( i + 1 ) : 0;
+    const numAsterisks = i * 2 + 1;
+    str += " ".repeat( numSpaces ) + "#".repeat( numAsterisks ) + " ".repeat( numSpaces );
+
+    console.log( str );
+  }
+}
 
 module.exports = pyramid;
