274fcc60e7ba7970da8ea92fa7a3a0e4cbaba6c9
evan bechtol <evanbechtol@gmail.com>
2020-08-07 15:42:47 -0500

    Weave Exercise
    - Implement weave

--- exercises/weave/index.js
+++ exercises/weave/index.js
@@ -3,7 +3,7 @@
 // 2) Implement the 'weave' function.  Weave
 // receives two queues as arguments and combines the
 // contents of each into a new, third queue.
-// The third queue should contain the *alterating* content
+// The third queue should contain the *alternating* content
 // of the two queues.  The function should handle
 // queues of different lengths without inserting
 // 'undefined' into the new one.
@@ -22,8 +22,22 @@
 //    q.remove() // 2
 //    q.remove() // 'There'
 
-const Queue = require('./queue');
+const Queue = require( "./queue" );
 
-function weave(sourceOne, sourceTwo) {}
+function weave ( sourceOne, sourceTwo ) {
+  const q = new Queue();
+
+  while ( sourceOne.peek() || sourceTwo.peek() ) {
+    if ( sourceOne.peek() ) {
+      q.add( sourceOne.remove() );
+    }
+
+    if ( sourceTwo.peek() ) {
+      q.add( sourceTwo.remove() );
+    }
+  }
+
+  return q;
+}
 
 module.exports = weave;
