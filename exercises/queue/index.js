41a68752bc4e586f192248e601c2caf6e08f5ce5
evan bechtol <evanbechtol@gmail.com>
2020-08-07 14:18:28 -0500

    Data Structure - Queue

--- exercises/queue/index.js
+++ exercises/queue/index.js
@@ -8,6 +8,18 @@
 //     q.add(1);
 //     q.remove(); // returns 1;
 
-class Queue {}
+class Queue {
+  constructor() {
+    this.data = [];
+  }
+
+  add (elem) {
+    this.data.unshift(elem);
+  }
+
+  remove() {
+    return this.data.pop();
+  }
+}
 
 module.exports = Queue;
