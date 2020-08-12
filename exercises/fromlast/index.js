acf9b5ac900c24b88cd5c1c33752530ae199ed0f
evan bechtol <evanbechtol@gmail.com>
2020-08-12 16:21:43 -0500

    fromLast Linked List Exercise
    - Implement `fromLast` method

--- exercises/fromlast/index.js
+++ exercises/fromlast/index.js
@@ -11,6 +11,23 @@
 //    list.insertLast('d');
 //    fromLast(list, 2).data // 'b'
 
-function fromLast(list, n) {}
+function fromLast ( list, n ) {
+  let slow = list.getFirst();
+  let fast = list.getFirst();
+
+  // Advance fast 'n' spaces
+  for (let i = 0; i < n; i++ ) {
+    if (fast.next) {
+      fast = fast.next;
+    }
+  }
+
+  while ( fast.next ) {
+    slow = slow.next;
+    fast = fast.next;
+  }
+
+  return slow;
+}
 
 module.exports = fromLast;
