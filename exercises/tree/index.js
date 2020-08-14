f698eae0cc89def8992805ab08017c9ff5975578
evan bechtol <evanbechtol@gmail.com>
2020-08-14 09:17:59 -0500

    Tree BFS
    - Implement method `traverseBF`

--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -26,6 +26,24 @@ class Node {
 }
 
 class Tree {
+  constructor () {
+    this.root = null;
+  }
+
+  traverseBF(fn) {
+    const arr = [this.root];
+
+    while(arr.length) {
+      const node = arr.shift();
+
+      arr.push(...node.children);
+      fn(node);
+    }
+  }
+
+  traverseDF() {
+
+  }
 }
 
 module.exports = { Tree, Node };
